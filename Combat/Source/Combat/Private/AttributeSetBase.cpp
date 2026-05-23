c6f6ddd263bc65c24a447e72389faa9ff0751ac3
Khoi Van <vanthekhoi@gmail.com>
2026-05-23 11:34:16 +0700

    [GAS] Add AttributeSetBase - Stamina

---
+++ Combat/Source/Combat/Private/AttributeSetBase.cpp
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AttributeSetBase.h"
+#include "Net/UnrealNetwork.h"
+#include "GameplayEffectExtension.h"
+
+UAttributeSetBase::UAttributeSetBase()
+{
+	Stamina = 100.f;
+}
+
+void UAttributeSetBase::OnRep_Stamina(const FGameplayAttributeData& OldStamina)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAttributeSetBase, Stamina, OldStamina);
+}
+
+void UAttributeSetBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAttributeSetBase, Stamina, COND_None, REPNOTIFY_Always);
+}
---
+++ Combat/Source/Combat/Public/AttributeSetBase.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSetBase.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class COMBAT_API UAttributeSetBase : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+	UAttributeSetBase();
+	
+	// Stamina attributes
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Stamina)
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS_BASIC(UAttributeSetBase, Stamina);
+
+	UFUNCTION()
+	virtual void OnRep_Stamina(const FGameplayAttributeData& OldStamina);
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+};
