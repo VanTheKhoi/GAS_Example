58cd26953204a83a494819732dca5270bba6ae4d
Khoi Van <vanthekhoi@gmail.com>
2026-06-13 12:43:35 +0700

    [CommonUI] Add CombatUserWidgetBase

---
+++ Combat/Source/Combat/Private/UI/CombatUserWidgetBase.cpp
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/CombatUserWidgetBase.h"
+#include "Components/Button.h"
+
+void UCombatUserWidgetBase::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (CloseUIButton)
+	{
+		CloseUIButton->OnClicked.AddDynamic(this, &UCombatUserWidgetBase::RemoveMainUI); // Callback
+	}
+}
+
+void UCombatUserWidgetBase::RemoveMainUI()
+{
+	RemoveFromParent();
+}
---
+++ Combat/Source/Combat/Public/UI/CombatUserWidgetBase.h
@@ -0,0 +1,30 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CombatUserWidgetBase.generated.h"
+
+/**
+ * 
+ */
+
+class UTextBlock;
+class UButton;
+
+UCLASS()
+class COMBAT_API UCombatUserWidgetBase : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UButton> CloseUIButton;
+	
+private:
+	UFUNCTION()
+	void RemoveMainUI();
+};
