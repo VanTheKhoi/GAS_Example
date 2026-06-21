b96ff1051ea52357a6ea5f0734a14f1d2b12419a
Khoi Van <vanthekhoi@gmail.com>
2026-06-21 14:50:43 +0700

    [BossFight] Add AbilityBase class

---
+++ Combat/Source/Combat/Public/Abilities/AbilityBase.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "AbilityBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBAT_API UAbilityBase : public UGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="UI")
+	bool bShowInContainner = true;
+};
